2a490fc95511dd39457de7f14c39a041116a0143
Filipe Ramos <filipemoaz@gmail.com>
2026-08-25 09:27:12 -0300

    Increase payload limit to 50mb for image uploads

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const pool = new Pool({
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('docs'));
 
 // EMPRESAS
